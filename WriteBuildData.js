b2e4866fe81ec6bbed6c5f4e4dfab60a06824ed5
Mark Ethan Trostler <mark@zzo.com>
2015-05-08 08:26:22 -0700

    write bucket files

--- WriteBuildData.js
+++ WriteBuildData.js
@@ -1,25 +1,33 @@
+'use strict';
+
 var Auth = require('./Auth');
 var Util = require('./Util');
 var gcloud = require('gcloud');
+var Q = require('q');
+var path = require('path');
 
 function WriteBuildData(repoName, githubEvent) {
   this.name = repoName;
-  this.bucketName = Util.makeBucketName(repoName);
   this.kind = githubEvent;
   this.namespace = Util.makeBucketName(repoName);
   this.dataset = gcloud.datastore.dataset(Auth);
+  this.storage = gcloud.storage(Auth);
 }
 
 WriteBuildData.prototype.getBucketName = function() {
-  return this.bucketName;
+  return this.namespace;
 };
 
-// Write out what happened 
+// Write out what happened
 WriteBuildData.prototype.store = function(buildInfo, rawBuildRequest, cb) {
   if (!this.buildId) {
     var me = this;
-    this.getNextBuildNumber(function(err, number) {
-      me.store(buildInfo, rawBuildRequest, cb);
+    this.getNextBuildNumber(function(err) {
+      if (err) {
+       cb(err);
+      } else {
+        me.store(buildInfo, rawBuildRequest, cb);
+      }
     });
   } else {
     var key = this.dataset.key({ namespace: this.namespace, path: [ this.kind, this.buildId ]});
@@ -34,7 +42,7 @@ WriteBuildData.prototype.getNextBuildNumber = function(cb) {
   var key = this.dataset.key({ namespace: this.namespace, path: [ 'buildId' ] });
   this.dataset.runInTransaction(function(transaction, done) {
     var query = me.dataset.createQuery(me.namespace, key.path);
-    transaction.runQuery(query, function(err, entities, endCursor, apiResponse) {
+    transaction.runQuery(query, function(err, entities) {
       if (err) {
         cb(err);
       } else {
@@ -50,7 +58,64 @@ WriteBuildData.prototype.getNextBuildNumber = function(cb) {
         done();
       }
     });
-  }, function(err) { cb(err, me.buildId); });
+  }, function(err) {
+    cb(err, me.buildId);
+  });
+};
+
+/*
+ * All files in fileList are stored in the 'prefix' directory in the bucket
+ */
+WriteBuildData.prototype.writeFilesToBucket = function(prefix, fileList, cb) {
+  var me = this;
+  // Array of function calls that return promises to save each file
+  var promises = fileList.map(function(file) {
+    return Q.ninvoke(me, 'writeFileToBucket', file, path.join(prefix, path.basename(file)));
+  });
+
+  // Execute and wait for all of them
+  Q.allSettled(promises).then(function(results) {
+
+    var successResponses =
+      results
+        .filter(function(result) {
+          return result.state === 'fulfilled';
+        })
+        .map(function(success) {
+          return success.value;
+        });
+
+    var failedResponses =
+      results
+        .filter(function(result) {
+          return result.state === 'rejected';
+        })
+        .map(function(failed) {
+          return failed.reason;
+        });
+
+    if (failedResponses.length) {
+      cb(failedResponses, successResponses);
+    } else {
+      cb();
+    }
+  });
+};
+
+WriteBuildData.prototype.writeFileToBucket = function(from, to, cb) {
+  var me = this;
+  var options = {
+    destination: to
+  };
+  this.storage.createBucket(this.getBucketName(), function(err, bucket) {
+    if (err) {
+      // Asume bucket already exists
+      // TODO(trostler): verify that's true!
+      bucket = me.storage.bucket(me.getBucketName());
+    }
+
+    bucket.upload(from, options, cb);
+  });
 };
 
 module.exports = WriteBuildData;
--- spec/WriteBuildData_spec.js
+++ spec/WriteBuildData_spec.js
@@ -1,7 +1,10 @@
 var WriteBuildData = require('../WriteBuildData');
+var path = require('path');
+var fs = require('fs');
+
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 
-describe("WriteBuildData", function() {
+describe("WriteBuildData datastore tests", function() {
   var wbd;
 
   beforeEach(function() {
@@ -29,9 +32,48 @@ describe("WriteBuildData", function() {
   });
 
   it("can store", function(done) {
-    wbd.store([ { a: 3} , {b:5}], { meta: 'data'}, function(err) {
+    wbd.store([{ a: 3 } , { b:5 }], { meta: 'data'}, function(err) {
       expect(err).toBeNull();
       done();
     });
+  });
+});
+
+
+describe("WriteBuildData bucket tests", function() {
+  var wbd;
+
+  beforeEach(function() {
+    wbd = new WriteBuildData('a/repo/name', 'push');
+  });
+
+  it("can write a file to a bucket", function(done) {
+    wbd.writeFileToBucket(
+      path.join(__dirname, 'WriteBuildData_spec.js'),
+      'test/foo/goo/WBDS.js',
+      function(err, ok) {
+        expect(err).toBeNull();
+        done();
+      }
+    );
+  });
+
+  it("can write files to a bucket", function(done) {
+    var files = fs.readdirSync(__dirname)
+      .map(function(file) {
+        return path.join(__dirname, file);
+      })
+      .filter(function(file) {
+        var stat = fs.lstatSync(file);
+        return stat.isFile() && file.match(/\.js$/);
+      });
+    wbd.writeFilesToBucket(
+      'path/to/files',
+      files,
+      function(err, ok) {
+        expect(err).toBeUndefined();
+        done();
+      }
+    );
   });
 });
